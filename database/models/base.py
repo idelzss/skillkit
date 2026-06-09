90c6358f754d93538baee79f96be4d83db9a54c3
Artem <artem.shesniak@gmail.com>
2026-06-09 13:59:34 +0300

    support postgresql database

--- database/models/base.py
+++ database/models/base.py
@@ -1,11 +1,17 @@
+import os
 from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DB_FILE = Path(__file__).resolve().parent.parent.parent / "database.db"
-
-ENGINE = create_engine(f"sqlite:///{DB_FILE}")
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL:
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    ENGINE = create_engine(DATABASE_URL)
+else:
+    DB_FILE = Path(__file__).resolve().parent.parent.parent / "database.db"
+    ENGINE = create_engine(f"sqlite:///{DB_FILE}")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=ENGINE)
 
@@ -14,3 +20,4 @@ BASE = declarative_base()
 
 def create_db():
     BASE.metadata.create_all(bind=ENGINE)
+
